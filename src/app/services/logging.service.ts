3a6eb6d092348aca7dff14127bcf0db78fa6b632
komirisetty <knagesh35@gmail.com>
2021-03-13 15:13:53 +0530

    Update logging.service.ts

--- src/app/services/logging.service.ts
+++ src/app/services/logging.service.ts
@@ -1,4 +1,7 @@
 import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
 
 @Injectable()
 export class MyMonitoringService {
